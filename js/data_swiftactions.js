f17474fd03d3276c4b85b555bcfa7741e7e0fba0
cylira <53834332+cylira@users.noreply.github.com>
2020-08-04 16:11:09 -0500

    Update data_swiftactions.js

--- js/data_swiftactions.js
+++ js/data_swiftactions.js
@@ -1,36 +1,22 @@
 data_swift = [
     {
-        title: "Ataque con dos armas",
-        icon: "crossed-swords",
-        subtitle: "Se usa con la acción de Ataque",
-        description: "Ataca con una arma en tu otra mano",
-        reference: "PHB, pgs. 192,194-195.",
+        title: "Cambiar cómo empuñas un arma",
+        icon: "hand",
+        subtitle: " ",
+        description: "Cambias el modo en que empuñas un arma.",
+        reference: "SFCR, Pág. 247.",
         bullets: [
-            "Sólo utilizable si tomas la acción de <i>Atacar</i> y atacas con un arma cuerpo a cuerpo ligera que estés sosteniendo en una mano.",
-            "Realiza un sólo ataque con una arma cuerpo a cuerpo ligera distinta que estés sosteniendo en la otra mano.",
-            "No agregas tu modificador de característica al daño del ataque adicional, salvo que este sea negativo.",
-            "Si alguna de las armas tiene la propiedad de arrojadiza, puedes lanzarla en lugar de hacer el ataque cuerpo a cuerpo con ella."
+            "Por ejemplo: Pasar de empuñar un arma a dos manos con ambas manos a sujetarla con una sola, es una acción rápida."
         ]
     },
     {
-        title: "Lanzar un conjuro",
-        icon: "magic-swirl",
-        subtitle: "Lanzamiento de 1 acción adicional",
-        description: "Lanza un conjuro con un tiempo de lanzamiento de 1 acción adicional",
-        reference: "PHB, pg. 192.",
+        title: "Tumbarse",
+        icon: "crawl",
+        subtitle: "Tirarse al suelo",
+        description: "Adoptar una posición tumbada en tu mismo espacio es una acción rápida.",
+        reference: "SFCR, Pág. 247.",
         bullets: [
-            "No puedes lanzar un conjuro con tu acción y un conjuro distinto con tu acción adicional en el mismo turno, salvo que la acción se use para lanzar un truco.",
-            "Para más detalles ve la accion de  <i>Lanzar un conjuro</i>."
-        ]
-    },
-    {
-        title: "Usar un rasgo de clase",
-        icon: "embrassed-energy",
-        subtitle: "Rasgos que utilizan 1 acción adicional",
-        description: "Utiliza un rasgo racial o de clase que use una acción adicional",
-        reference: "Revisa la página de tu clase para más información.",
-        bullets: [
-
+		
         ]
     }
 ]
