dde6fc38505c7cd65f16795213ba2995fa2cc6e0
cylira <53834332+cylira@users.noreply.github.com>
2020-08-07 14:44:50 -0500

    Update data_otheractions.js

--- js/data_otheractions.js
+++ js/data_otheractions.js
@@ -2,18 +2,18 @@ data_other = [
     {
         title: "Cháchara de combate",
         icon: "shouting",
-        subtitle: "Se usa con la acción de Ataque",
-        description: "Ataca con una arma en tu otra mano",
+        subtitle: "Hablar en combate",
+        description: "Puedes hablar en una medida razonable, a discreción del DJ, sin gastar ninguna acción, aunque no sea tu turno.",
         reference: "SFCR, pág. 249.",
         bullets: [
             ""
         ]
     },
     {
-        title: "Dejar cae un objeto",
+        title: "Dejar caer un objeto",
         icon: "drop-weapon",
-        subtitle: "Se usa con la acción de Ataque",
-        description: "Ataca con una arma en tu otra mano",
+        subtitle: "Que tengas en la mano",
+        description: "Puedes dejar uno o más objetos que tengas en tus manos en tu misma casilla o una adyacente en cualquier momento, sin gastar acciones.",
         reference: "SFCR, pág. 249.",
         bullets: [
             ""
@@ -21,32 +21,41 @@ data_other = [
     },
 	{
         title: "Preparar una acción",
-        icon: "clockwork",
-        subtitle: "Se usa con la acción de Ataque",
-        description: "Ataca con una arma en tu otra mano",
+        icon: "empty-hourglass",
+        subtitle: "Elige un desencadenante",
+        description: "Podrás preparate para llevar a cabo una acción cuando ocurra cierto desencadenante usando una acción estándar.",
         reference: "SFCR, pág. 249.",
         bullets: [
-            ""
+            "Decide una acción estándar, de movimiento o rápida y un desencadenante.",
+			"Cuando el desencadenante tiene lugar, puedes llevar a cabo la acción que elegiste.",
+			"Esto cambia tu valor de iniciativa por el actual para el resto del combate.",
+			"Si usaste una reación en el turno anterior y luego decidiste preparar una acción, sigues recuperando tu reacción al inicio de tu turno original, no cuando llevas a cabo la acción preparada.",
+			"Si tu acción preparada es puramente defensiva, como adoptar la acción de defensa total si un enemigo al que te enfrentas te dispara, ésta ocurre justo antes del suceso que la desencadena.",
+			"Si la acción preparada no es puramente defensiva, como disparar a un enemigo si éste te dispara, tiene lugar inmediatamente después del suceso desdencadenante.",
+			"Si vuelve a llegar tu turno y no se ha desencadenado la acción preparada, esta ación se pierde (aunque puedes volver a preparar la misma acción de nuevo)."
         ]
     },
 	{
         title: "Retrasar turno",
         icon: "clockwork",
-        subtitle: "Se usa con la acción de Ataque",
-        description: "Ataca con una arma en tu otra mano",
+        subtitle: "Actúas en otro momento",
+        description: "Si no estás seguro de qué hacer cuando llega tu turno, podrás retrasar el momento de llevar a cabo una acción hasta después de que otros personajes ejecuten su turno.",
         reference: "SFCR, pág. 249.",
         bullets: [
-            ""
+            "Debes declarar que retrasas tu turno antes de llevar a cabo ninguna acción en tu turno (esto no requiere que gastes ninguna de tus acciones).",
+			"Después de que cualquier criatura termine su turno en el orden de iniciariva, podrás dejar de retrasar y llevar a cabo tu turno.",
+			"Esto cambia tu valor de iniciativa por el actual para el resto del combate.",
+			"Si usaste una reacción en el turno anterior y luego decidiste retrasar tu turno, sigues recuperando tu reacción al inicio de tu turno original, no cuando dejas de retrasar el turno."
         ]
     },
 	{
         title: "Usar una habilidad",
-        icon: "crossed-swords",
-        subtitle: "Se usa con la acción de Ataque",
-        description: "Ataca con una arma en tu otra mano",
+        icon: "embrassed-energy",
+        subtitle: "De clase, raza u otro",
+        description: "Al igual que usar habilidades en diferentes circunstancias, usar una habilidad en combate normalmente (pero no siempre) requiere llevar a cabo una acción.",
         reference: "SFCR, pág. 249.",
         bullets: [
-            ""
-        ]
+            "La acción requerida al usar una habilidad depende de la misma y de la tarea específica que estás tratando de conseguir."
+			]
     },
 ]
